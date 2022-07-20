5801d7d5fbfef49574a2da12324aeadecf5fdde2
Vinicius Siqueira <vinicius_seek@yahoo.com>
2022-07-19 21:37:45 -0300

    Eliminação de redundância na importação da API BodyHandlers.

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.*;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
 
@@ -10,14 +10,14 @@ public class App {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
 
-        // estabelecer a url utilizando o  link contendo os dados da API em JSON
+        // estabelecer a url utilizando o link contendo os dados da API em JSON
         String url = "https://api.mocki.io/v2/549a5d8b";
-
-        // gerar uma URI para associar URL
-        URI address = URI.create(url);
 
         // criação de client
         HttpClient client = HttpClient.newHttpClient();
+
+        // gerar uma URI para associar URL
+        URI address = URI.create(url);
 
         // criação de request
         HttpRequest request = HttpRequest.newBuilder(address).GET().build();
